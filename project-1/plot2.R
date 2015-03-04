48e5a6e5bb55a44374fb4289516651addb168959
Hoang Chuong Tran <tranhoangchuongapcs@gmail.com>
2015-03-04 18:13:16 +0100

    finish plot 1,2,3. begin plot 4

--- project-1/plot2.R
+++ project-1/plot2.R
@@ -1,9 +1,4 @@
-library("ggplot2")
-library("scales")
-
 data <- read.csv("data.txt",na.strings="?",sep=";")
-#read.table("household_power_consumption.txt",header=TRUE,na.strings="?",sep=";")
-#data <- data[(data$Date=="1/2/2007" | data$Date=="2/2/2007" ),]
 
 # parse date + time string to DateTime type in R
 x <- paste(data$Date, data$Time)
@@ -15,10 +10,11 @@ filename <- "plot2.png"
 png(filename=filename, width=480, height=480, units="px")
 
 # plot
-ggplot(data=data, aes(x=Time, y=Global_active_power)) +
-        geom_path(aes(group=Date)) + 
-        ylab("Global Active Power (kilowatts)") + 
-        scale_x_datetime(breaks = date_breaks("1 day")) +
-        xlab(NULL)
+plot(x=data$Time,
+     y=data$Global_active_power,
+     type="l",
+     ylab="Global Active Power (kilowatts)",
+     xlab="")
+
 dev.off()
 
---
+++ project-1/plot3.R
@@ -0,0 +1,26 @@
+data <- read.csv("data.txt",na.strings="?",sep=";")
+
+# parse date + time string to DateTime type in R
+x <- paste(data$Date, data$Time)
+data$Date <- weekdays(as.Date(data$Date, format("%d/%m/%Y")))
+data$Time <- strptime(x, format("%d/%m/%Y %H:%M:%S"))
+
+# save plot to png
+filename <- "plot3.png"
+png(filename=filename, width=480, height=480, units="px")
+
+# plot
+plot(x=data$Time, y=data$Sub_metering_1,
+     type="l",
+     ylab="Energy sub metering",
+     xlab="",
+     col="black")
+lines(x=data$Time,y=data$Sub_metering_2,type="l",col="red")
+lines(x=data$Time,y=data$Sub_metering_3,type="l",col="blue")
+legend("topright",
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+       col=c("black","red","blue"),lty=1,
+       xjust=1)
+
+dev.off()
+
