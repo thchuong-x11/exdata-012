db94303e45abd9ee6774f3213b60fab8bdee66dd
Hoang Chuong Tran <tranhoangchuongapcs@gmail.com>
2015-03-04 13:35:42 +0100

    Plot 2
    Plot 2 finishes but I have problem making break label to be weekdays
    rather than the date

--- project-1/plot2.R
+++ project-1/plot2.R
@@ -1,17 +1,24 @@
 library("ggplot2")
-# read treated data
+library("scales")
+
 data <- read.csv("data.txt",na.strings="?",sep=";")
+#read.table("household_power_consumption.txt",header=TRUE,na.strings="?",sep=";")
+#data <- data[(data$Date=="1/2/2007" | data$Date=="2/2/2007" ),]
 
 # parse date + time string to DateTime type in R
-x <- paste(data[,1], data[,2])
-strptime(x, "%d/%m/%Y %H:%M:%S")
-data[,1] <- x
+x <- paste(data$Date, data$Time)
+data$Date <- weekdays(as.Date(data$Date, format("%d/%m/%Y")))
+data$Time <- strptime(x, format("%d/%m/%Y %H:%M:%S"))
 
 # save plot to png
 filename <- "plot2.png"
 png(filename=filename, width=480, height=480, units="px")
 
 # plot
-ae <- aes(x=Date, y=Global_active_power,group=Date)
-ggplot(data=data,ae) + geom_point(linetype="solid")
+ggplot(data=data, aes(x=Time, y=Global_active_power)) +
+        geom_path(aes(group=Date)) + 
+        ylab("Global Active Power (kilowatts)") + 
+        scale_x_datetime(breaks = date_breaks("1 day")) +
+        xlab(NULL)
 dev.off()
+
